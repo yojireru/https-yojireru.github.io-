40fd15c7d79beeba64877daf22b8d6d3009f1563
yojireru <iamdarkstar12@gmail.com>
2025-08-05 14:58:06 -0700

    test4

--- scripts.js
+++ scripts.js
@@ -1,20 +1,36 @@
-const galleryImages = document.querySelectorAll("#gallery img");
+const gallery = document.getElementById("gallery");
 const imageViewer = document.getElementById("imageViewer");
 const xButton = document.getElementById("xButton");
+const selectedImage = document.getElementById("selectedImage");
+const rect = selectedImage.getBoundingClientRect();
+let zoomed = false;
 
-galleryImages.forEach( (img) => {
-    img.addEventListener("click", () => {
-        imageViewer.style.visibility = "visible";
-        imageViewer.style.height = document.documentElement.scrollHeight + "px";
-        const image = imageViewer.querySelector("img");
-        image.src = img.src;
+const IMAGE_PATH = "img/";
+
+fetch("gallery.json")
+    .then(response => response.json())
+    .then(images => {
+        images.forEach(filename => {
+            const img = document.createElement("img");
+            img.src = IMAGE_PATH + filename;
+            gallery.appendChild(img);
+
+            img.addEventListener("click", () => {
+                imageViewer.style.height = document.documentElement.scrollHeight + "px";
+                selectedImage.src = img.src;
+                imageViewer.style.visibility = "visible";
+                // glue x button to top right corner
+                xButton.style.top = rect.top - selectedImage.height / 2 - xButton.height / 2 + "px";
+                xButton.style.left = rect.right - xButton.width / 2 + "px";
+            });
     });
-});
+})
+.catch(console.error);
 
 imageViewer.addEventListener("click", () => {
     imageViewer.style.visibility = "hidden";
 });
 
-imageViewer.querySelector("img").addEventListener("click", (e) => {
+selectedImage.addEventListener("click", (e) => {
     e.stopPropagation();
 });
